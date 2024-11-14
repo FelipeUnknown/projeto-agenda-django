ec2c73f1687a4625ac294eef46d566a245355caf
Felipe da Silva <felipedasilva1825@gmail.com>
2024-11-14 19:37:25 -0300

    criação do serializer pessoa e contato 14/11/2024

---
+++ api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from api.models import Pessoa, Contato
+
+
+class PessoaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pessoa
+        fields = (
+            'nome',
+            'endereco',
+            'bairro',
+            'cidade',
+            'uf'
+        )
+class ContatoSerializr(serializers.ModelSerializer):
+    class Meta:
+        model = Contato
+        fields =(
+            'pessoa',
+            'contato',
+            'tipo',
+            'descricao'
+        )
